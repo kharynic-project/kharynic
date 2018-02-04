52fbb33b5a69713eaf5a013a4a2c666ad0205c4a
Adam Golebiowski <Avallach7@users.noreply.github.com>
2018-02-04 15:32:05 +0100

    Updated .gitignore and cleanup.sh

--- dev-utils/cleanup.sh
+++ dev-utils/cleanup.sh
@@ -5,17 +5,28 @@ cd $( dirname "${BASH_SOURCE[0]}" )/..
 pwd
 echo
 rm -r engine/Temp
-rm -r engine/Library/metadata
-rm -r engine/Library/ShaderCache
-rm -r engine/Library/ScriptAssemblies
-rm -r engine/Library/obj
-rm    engine/Library/assetDatabase3
-rm    engine/Library/CurrentLayout.dwlt
-rm    engine/Library/CurrentMaximizeLayout.dwlt
-rm    engine/Library/expandedItems
-rm    engine/Library/LastSceneManagerSetup.txt
-rm    engine/Library/*.log
+rm -r engine/obj
+rm -r engine/Library
+rm -r engine/UnityPackageManager
 rm    engine/Assets/**/*.meta
+rm    engine/ProjectSettings/AudioManager.asset
+rm    engine/ProjectSettings/ClusterInputManager.asset
+rm    engine/ProjectSettings/DynamicsManager.asset
+rm    engine/ProjectSettings/EditorBuildSettings.asset
+rm    engine/ProjectSettings/InputManager.asset
+rm    engine/ProjectSettings/NavMeshAreas.asset
+rm    engine/ProjectSettings/NetworkManager.asset
+rm    engine/ProjectSettings/Physics2DSettings.asset
+rm    engine/ProjectSettings/TagManager.asset
+rm    engine/ProjectSettings/TimeManager.asset
+rm    engine/ProjectSettings/UnityConnectSettings.asset
+rm    engine/Assembly-CSharp.csproj
+rm    engine/engine.sln
+
+rm -r engine/.vscode
+rm -r engine/.idea
+rm    engine/Assembly-CSharp-Editor-firstpass.csproj
+rm    engine/Assets/Plugins/Editor/JetBrains
 echo
 echo finished
 sleep 1
