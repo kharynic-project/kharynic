9b36956bc56ee6110fa9d50f88bde4e1762091fd
Adam Golebiowski <Avallach7@users.noreply.github.com>
2018-02-10 19:54:10 +0100

    Added framerate logging

--- engine/Assets/UnityBridge.cs
+++ engine/Assets/UnityBridge.cs
@@ -1,22 +1,43 @@
-﻿namespace org.kharynic
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace org.kharynic
 {
-    public class UnityBridge : UnityEngine.MonoBehaviour
+    public class UnityBridge : MonoBehaviour
     {
-        [UnityEngine.RuntimeInitializeOnLoadMethod]
+        [RuntimeInitializeOnLoadMethod]
         public static void Main()
         {
-            var gameObject = new UnityEngine.GameObject(nameof(UnityBridge));
+            var gameObject = new GameObject(nameof(UnityBridge));
             gameObject.AddComponent<UnityBridge>();
         }
 
-        private void Start()
+        public void Start()
         {
-            Engine.Instance.Main(System.Environment.GetCommandLineArgs());
+            Engine.Instance.Main(Environment.GetCommandLineArgs());
+            StartCoroutine(LogFramerates(interval: TimeSpan.FromSeconds(3)));
         }
 
-        private void OnDestroy()
+        public void OnDestroy()
         {
             Engine.Instance.Dispose();
+        }
+        
+        IEnumerator LogFramerates(TimeSpan interval)
+        {
+            int lastFrameCount = 0;
+            float lastTotalTime = 0;
+            while (true)
+            {
+                var frameCount = Time.frameCount;
+                var totalTime = Time.realtimeSinceStartup;
+                var framerate = (frameCount - lastFrameCount) / (totalTime - lastTotalTime);
+                Debug.Log($"Framerate: {framerate:F1}fps");
+                lastFrameCount = frameCount;
+                lastTotalTime = totalTime;
+                yield return new WaitForSeconds((float)interval.TotalSeconds);
+            }
         }
     }
 }
