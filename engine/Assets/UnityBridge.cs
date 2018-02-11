e81bb29d94ba54abc5f31ee4ca2725fb824f35a4
Adam Golebiowski <Avallach7@users.noreply.github.com>
2018-02-11 10:19:10 +0100

    #graphics added automatic switch to high quality graphics if framerate high and stable enough

--- engine/Assets/UnityBridge.cs
+++ engine/Assets/UnityBridge.cs
@@ -23,11 +23,31 @@ namespace org.kharynic
         {
             Engine.Instance.Dispose();
         }
+
+        private bool _highGraphicSettings = false;
+        private int _graphicSettingsSwitchCount = 0;
+        private bool HighGraphicSettings
+        {
+            get { return _highGraphicSettings; }
+            set
+            {
+                if (value == _highGraphicSettings)
+                    return;
+                _graphicSettingsSwitchCount++;
+                Debug.Log($"Switching graphic settings to {(value ? "high" : "low")}");
+                _highGraphicSettings = value;
+                QualitySettings.shadows = value ? ShadowQuality.All : ShadowQuality.HardOnly;
+                QualitySettings.shadowResolution = value ? ShadowResolution.High : ShadowResolution.Low;
+                QualitySettings.shadowCascades = value ? 4 : 1;
+                QualitySettings.shadowDistance = value ? 30 : 10;
+                QualitySettings.antiAliasing = value ? 2 : 0;
+            }
+        }
         
         IEnumerator LogFramerates(TimeSpan interval)
         {
-            int lastFrameCount = 0;
-            float lastTotalTime = 0;
+            var lastFrameCount = 0;
+            var lastTotalTime = 0f;
             while (true)
             {
                 var frameCount = Time.frameCount;
@@ -36,6 +56,10 @@ namespace org.kharynic
                 Debug.Log($"Framerate: {framerate:F1}fps");
                 lastFrameCount = frameCount;
                 lastTotalTime = totalTime;
+                if (framerate > 50 && _graphicSettingsSwitchCount < 3)
+                    HighGraphicSettings = true;
+                else if (framerate < 30)
+                    HighGraphicSettings = false;
                 yield return new WaitForSeconds((float)interval.TotalSeconds);
             }
         }
