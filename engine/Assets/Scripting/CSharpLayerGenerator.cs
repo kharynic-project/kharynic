be0b97c7381589956d4d181008d9f05659b78453
Adam Golebiowski <Avallach7@users.noreply.github.com>
2018-02-26 20:31:22 +0100

    #engine scripting system refactoring

---
+++ engine/Assets/Scripting/CSharpLayerGenerator.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+namespace org.kharynic.Scripting
+{
+    // Generates C# glue code for calling .net methods from scripts.
+    // TODO: support for reference parameters and return types
+    internal class CSharpLayerGenerator : GeneratorBase
+    {
+        private const string GeneratedInterfaceSuffix = "_generated";
+
+        public CSharpLayerGenerator(Type targetType, string rootNamespace) 
+            : base(targetType, rootNamespace)
+        {
+        }
+
+        protected override string GenerateCode()
+        {
+            var header =
+                $"namespace {TargetType.Namespace}\n" +
+                $"{{\n" +
+                $"    {GeneratorUtils.GetHeaderComment()}\n" +
+                $"    [{typeof(GeneratedInterfaceAttribute).FullName}]\n" +
+                $"    public class {TargetType.Name}{GeneratedInterfaceSuffix}\n" +
+                $"    {{\n";
+            var footer =
+                $"    }}\n" +
+                $"}}\n";
+            return
+                header +
+                string.Join("\n", Methods.Select(GenerateMethodWrapper)) +
+                footer;
+        }
+
+        protected override string GetPath()
+        {
+            return new Regex($"^{RootNamespace}")
+                       .Replace(TargetType.Namespace ?? "", "")
+                       .Replace(".", "/") + $"/{TargetType.Name}.generated.cs";
+        }
+
+        private string GenerateMethodWrapper(MethodInfo method)
+        {
+            var @params = GetParams(method);
+            var paramDeclarations = @params.Select(p => $"{p.Type.FullName} {p.Name}");
+            var paramList = string.Join(", ", paramDeclarations);
+            var returnType = method.ReturnType == typeof(void) ? "void" : method.ReturnType.FullName; // CS0673 fix
+            var delegateDeclaration = 
+                $"        public delegate {returnType} {method.Name}{nameof(Delegate)}({paramList});\n";
+            var header =
+                $"        [{typeof(AOT.MonoPInvokeCallbackAttribute).FullName}(typeof({method.Name}{nameof(Delegate)}))]\n" +
+                $"        public static {returnType} {method.Name}({paramList})\n" +
+                $"        {{\n";
+            const string thisRefVar = "thisRef";
+            const string thisHandleVar = "thisHandle";
+            var thisRefDeclaration = method.IsStatic ? "" :
+                $"            var {thisHandleVar} = {typeof(GCHandle).FullName}.{nameof(GCHandle.FromIntPtr)}({ThisPtrVar});\n" +
+                $"            var {thisRefVar} = ({TargetType.FullName}) {thisHandleVar}.{nameof(GCHandle.Target)};\n";
+            var stringParams = method.GetParameters().Where(p => p.ParameterType == typeof(string));
+            var stringParamDeclarations = string.Join("", stringParams.Select(p =>
+                $"            var {p.Name} = {typeof(Marshal).FullName}.{nameof(Marshal.PtrToStringAuto)}({p.Name}{PtrSuffix});\n"));
+            var callSubject = method.IsStatic ? TargetType.FullName : thisRefVar;
+            var argsList = string.Join(", ", method.GetParameters().Select(p => p.Name));
+            var returnStatement = method.ReturnType != typeof(void) ? "return " : "";
+            var call =
+                $"            {returnStatement}{callSubject}.{method.Name}({argsList});\n";
+            var footer = 
+                $"        }}\n";
+            return
+                delegateDeclaration +
+                header +
+                thisRefDeclaration + 
+                stringParamDeclarations + 
+                call + 
+                footer;
+        }
+        
+        // returns list of params with references changed to pointers and optional "this" pointer
+        private static IEnumerable<VarInfo> GetParams(MethodBase method)
+        {
+            var thisParam =
+                method.IsStatic
+                    ? Enumerable.Empty<VarInfo>()
+                    : new[] {new VarInfo {Name = ThisPtrVar, Type = typeof(IntPtr)}};
+            return thisParam.Concat(
+                method.GetParameters()
+                    .Select(p =>
+                        p.ParameterType.IsPrimitive
+                            ? new VarInfo {Name = p.Name, Type = p.ParameterType}
+                            : new VarInfo {Name = p.Name + PtrSuffix, Type = typeof(IntPtr)}))
+                .ToArray();
+        }
+    }
+}
--- engine/Assets/Scripting/Runtime.cs
+++ engine/Assets/Scripting/Runtime.cs
@@ -8,6 +8,7 @@ namespace org.kharynic.Scripting
 {
     // Use this class to gather runtime method data from generated code and pass it to scripts.
     // They already have generated wrappers, but function pointers are needed to run them.
+    // This class has JavaScript part (/scripts/Engine/Scripting/Runtime.js).
     public static class Runtime
     {
         private const BindingFlags MethodBindingFlags = BindingFlags.Public |
