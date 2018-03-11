7e37a9a4216ed001aa3ed121f9e2c0668cc156c9
Adam Golebiowski <Avallach7@users.noreply.github.com>
2018-03-11 10:22:38 +0100

    #engine added Resources for asynchronous asset loading

---
+++ Engine/Assets/Resources.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Kharynic.Engine.Unity;
+
+namespace Kharynic.Engine
+{
+    public class Resources
+    {
+        private static readonly Lazy<string> LazyGameUrl = new Lazy<string>(() => {
+            if (BuildInfo.IsWebApp)
+                return WebHost.EngineInterface.GetRootUrl();
+            var fileProtocol = BuildInfo.LocalProjectPath.Contains(":") ? "file:///" : "file://";
+            return $"{fileProtocol}{BuildInfo.LocalProjectPath}";
+        });
+
+        private static string GameUrl => LazyGameUrl.Value;
+
+        private static string ToUrl(string projectRelativePath) => $"{GameUrl}/{projectRelativePath}";
+
+        public static async Task<string> LoadText(string path)
+        {
+            using (var www = new UnityEngine.WWW(ToUrl(path)))
+            {
+                await www;
+                if (!string.IsNullOrEmpty(www.error) || string.IsNullOrEmpty(www.text))
+                    throw new WebException(www.error);
+                return www.text;
+            }
+        }
+    }
+}
