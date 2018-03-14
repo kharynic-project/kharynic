e5cee4bc708ea6b6ee42ff044fb26f5d1ef8fd1f
Adam Golebiowski <Avallach7@users.noreply.github.com>
2018-03-14 09:09:05 +0100

    #engine WorldObject added Remove() and hanged logging

--- Engine/Assets/WorldObject.cs
+++ Engine/Assets/WorldObject.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.IO;
-using System.Threading.Tasks;
 using Kharynic.Engine.Scripting;
 using Kharynic.Engine.Unity;
 using UnityEngine;
 using UnityEngine.Rendering;
+using Object = UnityEngine.Object;
 
 namespace Kharynic.Engine
 {
@@ -58,6 +58,12 @@ namespace Kharynic.Engine
         }
 
         [Scriptable]
+        public void Remove()
+        {
+            Object.Destroy(_gameObject);
+        }
+
+        [Scriptable]
         public void SetRotation(float x, float y, float z)
         {
             _gameObject.transform.eulerAngles = new Vector3(x,y,z);
@@ -65,21 +71,17 @@ namespace Kharynic.Engine
 
         private async void LoadMesh(string meshPath)
         {
-            var name = Path.GetFileNameWithoutExtension(meshPath);
-            Debug.Log($"loading mesh {name}: started");
+            Debug.Log($"LoadMesh({meshPath})");
             _gameObject.transform.localScale = new Vector3(-1, 1, 1);
             var meshFilter = _gameObject.AddComponent<MeshFilter>();
             var startTime = DateTime.Now;
             var objDef = await Resources.LoadText(meshPath);
-            Debug.Log($"loading mesh {name}: file loaded");
             var objMesh = await ObjMesh.Load(objDef);
-            Debug.Log($"loading mesh {name}: file parsed");
             var unityMeshData = await UnityMeshData.Load(objMesh);
-            Debug.Log($"loading mesh {name}: mesh processed");
             meshFilter.mesh = unityMeshData.ToUnityMesh();
 
             var time = (DateTime.Now - startTime).TotalSeconds;
-            Debug.Log($"loading mesh {name}: finished after {time}s ({objDef.Length} bytes, " +
+            Debug.Log($"LoadMesh({meshPath}): finished after {time}s ({objDef.Length} bytes, " +
                       $"{unityMeshData.TriangleCount/3} tris, {unityMeshData.VertexCount} verts)");
         }
     }
